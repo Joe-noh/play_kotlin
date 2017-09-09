d7e35f92cd0ff816f6549fca210641e5e3bcbbd4
Joe-noh <goflb.jh@gmail.com>
2017-09-09 13:57:10 +0900

    FizzBuzz judge returns string instead printing

--- src/FizzBuzz.kt
+++ src/FizzBuzz.kt
@@ -1,12 +1,12 @@
 package play_kotlin
 
 class FizzBuzz {
-    fun judge(n: Int) {
-        when {
-            n % 15 == 0 -> println("FizzBuzz")
-            n % 5 == 0 -> println("Buzz")
-            n % 3 == 0 -> println("Fizz")
-            else -> println(n)
+    fun judge(n: Int): String {
+        return when {
+            n % 15 == 0 -> "FizzBuzz"
+            n % 5 == 0 -> "Buzz"
+            n % 3 == 0 -> "Fizz"
+            else -> n.toString()
         }
     }
 }
@@ -15,6 +15,6 @@ fun main(args: Array<String>) {
     val fb = FizzBuzz()
 
     for (i in 1..30) {
-        fb.judge(i)
+        println(fb.judge(i))
     }
 }
