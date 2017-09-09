cea95af4a80eddcf0c39d8f786b5fe41be1293e4
Joe-noh <goflb.jh@gmail.com>
2017-09-09 16:12:07 +0900

    use it

--- src/test/kotlin/FizzBuzzSpec.kt
+++ src/test/kotlin/FizzBuzzSpec.kt
@@ -1,5 +1,6 @@
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 import org.junit.Assert.assertEquals
 import play_kotlin.FizzBuzz
 
@@ -7,10 +8,12 @@ object FizzBuzzSpec : Spek({
     describe("judge") {
         val fb = FizzBuzz()
 
-        assertEquals("1",    fb.judge(1))
-        assertEquals("2",    fb.judge(2))
-        assertEquals("Fizz", fb.judge(3))
-        assertEquals("4",    fb.judge(4))
-        assertEquals("Buzz", fb.judge(5))
+        it("follows the famous rule") {
+            assertEquals("1", fb.judge(1))
+            assertEquals("2", fb.judge(2))
+            assertEquals("Fizz", fb.judge(3))
+            assertEquals("4", fb.judge(4))
+            assertEquals("Buzz", fb.judge(5))
+        }
     }
 })
